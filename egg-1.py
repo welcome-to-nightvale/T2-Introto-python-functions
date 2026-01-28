5e65b7f8b28ecce01a52ee6a16d47b6149893bdb
screaming in agony <ameliar51@nycstudents.net>
2026-01-28 10:17:21 -0500

    Create egg-1.py

---
+++ egg-1.py
@@ -0,0 +1,45 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+""" t.forward(200) """
+
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200) """
+
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) """
+
+""" def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+""" def rect():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rect() """
+
+turtle.done()
