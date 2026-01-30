1af4c3102adf3e911da7844431ea021ac1e89085
screaming in agony <ameliar51@nycstudents.net>
2026-01-29 21:54:58 -0500

    sobbing

--- egg-2.py
+++ egg-2.py
@@ -14,13 +14,71 @@ def square(x,y):
         t.left(y)
 square(100,90) """
 
-for i in range(60):
+""" for i in range(60):
+    side = 100
+    rotate = 5
+    def square(x,y):
+        for i in range(4):
+            t.forward(x)
+            t.left(y)
+    square(100,90)
+    t.right(5) """
+
+""" for i in range (4):
+    t.forward(100)
+    t.left(90) """
+
+""" for i in range(60):
     side = 100
     rotate = 90
     def square(x,y):
-            for i in range(4):
-                t.forward(x)
-                t.left(y)
-    square(100,90)
+        for i in range(4):
+            t.forward(x)
+            t.left(y)
+    triangle(100,90) """
+
+""" def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+
+def squares(iRange):
+    x = 25
+    for i in range(iRange):
+        square(x, 90)
+        x += 25
+squares(5) """
+
+""" sidelength = 100
+rotate = 90
+def triangle (x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+triangle(100,90) """
+
+""" for i in range(60):
+    side = 50
+    rotate = 90
+    def square(x,y):
+        x = 50
+        for i in range(4):
+            t.forward(x) 
+            t.left(y)
+    square(50,90) """
+
+""" def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+
+def squares(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length += 5
+squares(5,90)
+ """
+
 
 turtle.done()
